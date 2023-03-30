a164f2cdf6fee43f8c773f261149ec1c13d64f9f
Fábián Balázs <fabianbal500@gmail.com>
2023-03-30 21:12:43 +0200

    print issue on 'elerheto.html' resolved

--- assets/JS/print.js
+++ assets/JS/print.js
@@ -1,10 +1,5 @@
 const printBtn = document.getElementById('printbtn');
-const printBtne = document.getElementById('printbtne');
 
 printBtn.addEventListener('click', function() {
-    print();
-});
-
-printBtne.addEventListener('click', function() {
     print();
 });
---
+++ assets/JS/printe.js
@@ -0,0 +1,5 @@
+const printBtne = document.getElementById('printbtne');
+
+printBtne.addEventListener('click', function() {
+    print();
+});
