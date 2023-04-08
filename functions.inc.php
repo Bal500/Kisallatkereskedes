a76f0b6fbbec88f02a3641e517bb357836631c8d
Fábián Balázs <fabianbal500@gmail.com>
2023-04-08 18:29:58 +0200

    minor bug fixed

--- functions.inc.php
+++ functions.inc.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     require_once 'dbh.inc.php';
     function emptyInputSignup($name, $email, $password, $password2) {
         $result = false;
